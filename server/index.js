f47cc87d9cb26fe23efbd75fbf8972c26da155e9
Alex Paul <al.9thousand@gmail.com>
2020-06-29 23:35:40 -0400

    back to where i was before

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 //body parsing middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 //server
@@ -12,6 +12,12 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => res.send('sup World!'))
 
 // matches all requests to /api
-app.use('/api', require('./api')) 
+//app.use('/api', require('./api')) 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(function(err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+app.listen(port, () => console.log(`Price Checker Server listening at http://localhost:${port}`))
